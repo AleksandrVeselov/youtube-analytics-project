5e6bc2f473a5b028687632973947b1f40f4dbae3
Aleksandr Veselov <sanek_ves@mail.ru>
2023-03-12 22:59:09 +0300

    added attributes into class Channel/добавлены атрибуты в класс Channel

--- src/channel.py
+++ src/channel.py
@@ -12,8 +12,16 @@ class Channel:
         self.channel_id = channel_id
         self.youtube = build('youtube', 'v3', developerKey=Channel.API_KEY)
         self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.url = f'https://www.youtube.com/channel/{self.channel_id}'
+        self.subscribers_count = self.channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        self.views_count = self.channel['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(json.dumps(self.channel, indent=2, ensure_ascii=False))
 
+vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
+vdud.print_info()
