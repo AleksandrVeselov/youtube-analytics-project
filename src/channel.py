13014f28149d1f96a373417c8e02636f09c9989c
Aleksandr Veselov <sanek_ves@mail.ru>
2023-03-13 22:42:29 +0300

    write methods print_info and to_json/написаны методы print_info и to_json

--- src/channel.py
+++ src/channel.py
@@ -6,12 +6,12 @@ from googleapiclient.discovery import build
 class Channel:
     """Класс для ютуб-канала"""
     API_KEY: str = os.getenv('YOUTUBE_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=API_KEY)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.__channel_id = channel_id
-        self.youtube = build('youtube', 'v3', developerKey=Channel.API_KEY)
-        self.channel = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        self.channel = Channel.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
         self.title = self.channel['items'][0]['snippet']['title']
         self.description = self.channel['items'][0]['snippet']['description']
         self.url = f'https://www.youtube.com/channel/{self.__channel_id}'
@@ -19,12 +19,22 @@ class Channel:
         self.video_count = self.channel['items'][0]['statistics']['videoCount']
         self.views_count = self.channel['items'][0]['statistics']['viewCount']
 
+    @classmethod
+    def get_service(cls):
+        """Возвращает объект для работы с API вне класса"""
+        return cls.youtube
+
+    @property
+    def channel_id(self):
+        """Свойство для обращения к приватному атрибуту __channel_id"""
+        return self.__channel_id
+
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(json.dumps(self.channel, indent=2, ensure_ascii=False))
 
-    @property
-    def channel_id(self):
-        return self.__channel_id
-vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
-vdud.print_info()
+    def to_json(self, file_name: str) -> None:
+        """Запись информации о канале в file_name.json"""
+        with open(file_name, 'w') as json_file:
+            json.dump(self.channel, json_file, ensure_ascii=False)
+
